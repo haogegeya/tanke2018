18fc4926eb5cdc957e2892fae5867693a964c157
haogege <2554084474@qq.com>
2018-10-06 12:38:16 +0800

    game fenkai

--- game_client.py
+++ game_client.py
@@ -8,17 +8,31 @@ from time import sleep
 from threading import Thread
 c=socket()
 c.connect(("127.0.0.1",6996))
+fa1,fa2=Pipe()
+fb1,fb2=Pipe()
+x=random.randint(0,800)
+y=random.randint(0,600)
 name=input("输入昵称：")
 while True:
     c.send(name.encode())
     data=c.recv(128)
     if data==b"OK":
+        name_shuju=(name,x,y)
         break
     else:
         name=input("重复了，重新输入：")
 
-fa1,fa2=Pipe()
-fb1,fb2=Pipe()
+
+# background_image_filename="315464.jpg"
+mouse_image_filename="0.png"
+pygame.init()
+screen=pygame.display.set_mode((800,600),0,32)
+pygame.display.set_caption("hello timer")
+SCREEN_COLLER=(255,255,255)
+# Abackground=pygame.image.load(background_image_filename).convert()
+mouse_cursor=pygame.image.load(mouse_image_filename).convert_alpha()
+
+
 def shuju_f(c):
     while True:
         # x=random.randrange(100)
@@ -36,43 +50,51 @@ def shuju_s(c):
         name=data[0]
         x=data[1]
         y=data[2]
-        fb2.send((x,y))
+        fb2.send((name,x,y))
         print((name,x,y))
+def shuju():
+    while True:
+        global x,y,name_shujue
+        x,y=pygame.mouse.get_pos()
+        fa2.send((x,y))
+        data=fb1.recv()
+        name=data[0]
+        x=int(data[1])
+        y=int(data[2])
+        
+
+
 def game_main():
-    background_image_filename="315464.jpg"
-    mouse_image_filename="0.png"
-    pygame.init()
-    screen=pygame.display.set_mode((800,600),0,32)
-    pygame.display.set_caption("hello timer")
-    # Abackground=pygame.image.load(background_image_filename).convert()
-    mouse_cursor=pygame.image.load(mouse_image_filename).convert_alpha()
     while True:
         for event in pygame.event.get():
             if event.type==QUIT:
                 exit()
-        # screen.blit(Abackground,(0,0))
-        screen.fill((255,255,255))
-
-        x,y=pygame.mouse.get_pos()
-        fa2.send((x,y))
-        data=fb1.recv()
-        x=int(data[0])
-        y=int(data[1])
-        x-=mouse_cursor.get_width()/2
-        y-=mouse_cursor.get_height()/2
-
+        
         screen.blit(mouse_cursor,(x,y))
-
-        pygame.display.update()
         sleep(0.05)
 
 
 p1=Process(target=shuju_f,args=(c,))
 p2=Process(target=shuju_s,args=(c,))
-p3=Process(target=game_main)
 p1.start()
 p2.start()
-p3.start()
+
+t=Thread(target=shuju)
+t.daemon=True
+t.start()
+t1=Thread(target=game_main)
+t1.daemon=True
+t1.start()
+
+while True:
+    # e.wait()
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            exit()
+    pygame.display.update()
+    screen.fill(SCREEN_COLLER)
+    sleep(0.05)
+
 # p1.join()
 # p2.join()
 # p3.join()
